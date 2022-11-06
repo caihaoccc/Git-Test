29b361a1ea9b3f0a8530de752cadd1d95a38e58f
Cai <2945245791@qq.com>
2022-11-06 15:30:18 +0800

    First Commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description TODO
+ * @Author Cai
+ * @Date 2022/11/6 15:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git！！！");
+    }
+}
